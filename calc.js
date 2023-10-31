26df453cebdf6d4aa1051e1a19419c1c6d6c6ce0
Scarletbobcat <tien.hoan314@yahoo.com>
2023-10-31 17:15:56 -0400

    ui looks like apple iphone calculator

--- calc.js
+++ calc.js
@@ -1,5 +1,8 @@
 // getting reference to the form by id
-const form = document.getElementById("form")
+const form = document.getElementById("inputs")
+let num1 = 0, num2, operator
+let buttons = document.getElementsByTagName("button")
+let fNum = true
 
 // here, we wait for the "compute" button to be pressed before anything is executed
 // we pass e(event object) which allows us access to prevent the default action
@@ -7,43 +10,66 @@ form.addEventListener("submit", (e) => {
     // 
     e.preventDefault()
 
-    // getting the values of the form
-    // *notice that we must parse the number inputs because they are input as strings
-    let num1 = parseInt(document.getElementById("first_num").value)
-    let num2 = parseInt(document.getElementById("second_num").value)
-    let operator = document.getElementById("operation").value
-    let answer = document.getElementById("answer")
 
-    // this will reset the answer cell to empty
-    answer.innerHTML = "Answer: "
-    let result
-    
-    // checking if either one is not a number
-    if (isNaN(num1) || isNaN(num2)) {
-        alert("First or Second number is not a number")
-    } else {
-        // take action depending on what the operator is
-        switch(operator){
-            case '+':
-                result = num1+num2
-                answer.innerHTML += " " + result
-                break
-            case '-':
-                result = num1-num2
-                answer.innerHTML += " " + result
-                break
-            case '*':
-                result = num1*num2
-                answer.innerHTML += " " + result
-                break
-            case '/':
-                if (num2 != 0){
-                    result = num1/num2
-                    answer.innerHTML += " " + result
-                } else {
-                    alert("Error - cannot divide by 0")
-                }
-                break
-        }
-    }
 })
+
+function clearScreen() {
+    num1 = 0
+    num2 = null
+    operator = null
+    answer.innerHTML = "0"
+    fNum = true
+}
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(e) {
+        e.preventDefault()
+        console.log(num1 ,"\n", num2)
+        if(!isNaN(parseInt(buttons[i].innerHTML))) {
+            if (answer.innerHTML === "0" || answer.innerHTML === "-0") {
+                let answer = document.getElementById("answer")
+                answer.innerHTML = parseInt(buttons[i].innerHTML)
+            } else {
+                answer.innerHTML += parseInt(buttons[i].innerHTML)
+            }
+        } else {
+
+            operator = buttons[i].innerHTML
+
+            switch (operator) {
+                case 'AC':
+                    clearScreen()
+                    break
+                case '+/-':
+                    if(fNum) {
+                        num1 = parseFloat(answer.innerHTML)
+                        answer.innerHTML = "-" + num1
+                        num1 = -num1
+                    } else {
+                        num2 = parseFloat(answer.innerHTML)
+                        answer.innerHTML = "-" + num2
+                        num2 = -num2
+                    }
+                    break
+                case '%':
+                    if(fNum) {
+                        num1 = parseFloat(answer.innerHTML)/100
+                        answer.innerHTML = num1
+                    } else {
+                        num2 = parseFloat(answer.innerHTML)/100
+                        answer.innerHTML = num2
+                    }
+                    break
+                case '&#xF7':
+
+                case 'x':
+                case '-':
+                case '+':
+                    num1 += parseFloat(answer.innerHTML)
+                    answer.innerHTML = num1
+                case '=':
+                case '.':
+            }
+        }
+    })
+}
