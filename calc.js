a3ee505647fcf9fd2a9fc3eea8d4fe4d1900ae55
Scarletbobcat <tien.hoan314@yahoo.com>
2023-11-01 11:55:43 -0400

    added comments

--- calc.js
+++ calc.js
@@ -1,26 +1,37 @@
+// class with all necessary functions of a calculator
 class Calculator {
+    // constructor calls the clear function
     constructor() {
         this.clear()
     }
 
+    // clear sets all operands to empty and the operation to null
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = null
     }
 
+    // adds the number pressed to the end of the currentOperand string
+    // only allows one decimal to be included
     appendNumber(number) {
         if(this.currentOperand.includes('.') && number === '.') 
             return
         this.currentOperand += number.toString()
     }
 
+    // selects the operand 
     chooseOperation(operand) {
+        // if currentOperand is empty then don't do anything
         if(this.currentOperand === '') return
+        // allows us to continuously compute without having to press equal each time
         if(this.previousOperand !== '') {
             this.compute()
         }
+        // saves operand into operation
         this.operation = operand.toString()
+        // allows the operation to convert the currentOperand to negative or a percentage of 100
+        // not fully functional yet
         if (this.operation === '%' || this.operation === '+/-') {
             this.compute()
         }
@@ -28,20 +39,28 @@ class Calculator {
         this.currentOperand = ''
     }
 
+    // updates the screen of the calculator to display currentOperand
     updateDisplay() {
+        if (this.currentOperand === 'Infinity') {
+            answer.innerHTML = 'Error'
+            return
+        }
         answer.innerHTML = this.currentOperand
     }
 
+    // does the calculation depending on the operand
     compute() {
         console.log(this.currentOperand)
         console.log(this.previousOperand)
         console.log(this.operation)
+
         let result
         let prev = parseFloat(this.previousOperand)
         let curr = parseFloat(this.currentOperand)
         if ((isNaN(prev) || isNaN(curr)) && !(this.operation === '%' || this.operation === '+/-')) {
             return
         }
+
         switch(this.operation) {
             case '+':
                 result = prev + curr
@@ -71,15 +90,17 @@ class Calculator {
     }
 }
 
-// getting reference to the form by id
+// getting reference to all buttons as well as output screen
 const numberButtons = document.querySelectorAll("[data-number]")
 const operationButtons = document.querySelectorAll("[data-operation]")
 const equalsButton = document.querySelector("[data-equals]")
 const allClearButton = document.querySelector("[data-all-clear]")
 let answer = document.getElementById("answer")
 
+// creating new instance of Calculator object
 const calculator = new Calculator()
 
+// create addEventListener for each number button that will append the number to currentOperand and update display
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         calculator.appendNumber(button.innerHTML)
@@ -87,6 +108,7 @@ numberButtons.forEach(button => {
     })
 })
 
+// create addEventListener for each operation button that will set the operation to the operand and update display
 operationButtons.forEach(button => {
     button.addEventListener("click", () => {
         calculator.chooseOperation(button.innerHTML)
@@ -94,11 +116,13 @@ operationButtons.forEach(button => {
     })
 })
 
+// if all clear button is pressed, call clear function and update display
 allClearButton.addEventListener("click", button => {
     calculator.clear()
     calculator.updateDisplay()
 })
 
+// if equals is pressed, compute the result and update display
 equalsButton.addEventListener("click", button => {
     calculator.compute()
     calculator.updateDisplay()
